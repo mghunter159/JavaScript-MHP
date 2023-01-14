c415b95b43dadcca995a9e99244769b3b6a4cce8
mghunter159 <ya@mghunter.ru>
2023-01-14 19:43:00 +0500

    Lesson-2

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-console.log('Hello, people')
-console.log(2022)
-console.log(2+2)
+console.log(05);
+console.log('Добро ' + 'пожаловать ' + 'на курс ' + 'Александр');
+
+//var date = new Date ();;
+//alert( date.toLocaleDateString() );
+
+document.getElementById("one").innerHTML = "Hello world";
+
+
+console.log(typeof undefined);
+console.log(typeof 0);
+console.log(typeof 10n);
+console.log(typeof true);
+console.log(typeof "Planet");
+console.log(typeof Symbol("id"));
+console.log(typeof null);
