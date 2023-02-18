010c37b8674ae6269ff608a52c3764eaec37f3bc
mghunter159 <ya@mghunter.ru>
2023-02-18 15:06:56 +0500

    Urok 8

--- main.js
+++ main.js
@@ -1,53 +1,30 @@
-let a = "Значение из переменной А";
-let clcbtn = document.querySelector(".func");
+let button1 = document.querySelector('.button1');
+let button2 = document.querySelector('.button2');
+let button3 = document.querySelector('.button3');
 
-function one() {
-    let div = document.querySelector('.func_out');
-    div.innerHTML = a;
-}
-clcbtn.onclick = one;
-
-
-//calc
-
-let inputCalc = document.querySelector('.calcinp');
-let btnPlus = document.querySelector('.calcplus');
-let btnMin = document.querySelector('.calcmin');
-let btnDel = document.querySelector('.calcdel');
-let btnUmn = document.querySelector('.calcumn');
-
-btnPlus.onclick = function () {
-    
-    let inp = Number(inputCalc.value);
-    let outp = inp + 3;
-
-    document.getElementById('outp').value = outp;
-    console.log(outp);
+button1.onclick = function () {
+    let div_btn1 = document.querySelector(".div_button1");
+    div_btn1.style.height = "200px";
+    div_btn1.style.width = "200px";
 }
 
-btnMin.onclick = function () {
-    
-    let inp = Number(inputCalc.value);
-    let outp = inp - 5;
-
-    document.getElementById('outp').value = outp;
-    console.log(outp);
+button2.onclick = function () {
+    let div_btn2 = document.querySelector(".div_button1");
+    div_btn2.style.fontFamily = "sans-serif";
+    div_btn2.style.color = "red";
+    div_btn2.style.fontSize = "8px";
 }
 
-btnDel.onclick = function () {
-    
-    let inp = Number(inputCalc.value);
-    let outp = inp / 10;
-
-    document.getElementById('outp').value = outp;
-    console.log(outp);
-}
-
-btnUmn.onclick = function () {
-    
-    let inp = Number(inputCalc.value);
-    let outp = inp * 22;
-
-    document.getElementById('outp').value = outp;
-    console.log(outp);
+button3.onclick = function () {
+    let div_btn3 = document.querySelector(".div_button1");
+    let num = Math.floor(Math.random() * 3);
+    console.log(num);
+    switch(num) {
+    case(0):
+        div_btn3.style.removeProperty("fontFamily");
+    case(1):
+        div_btn3.style.removeProperty("color");
+    case(2):
+        div_btn3.style.removeProperty("font-Size");
+    }
 }
