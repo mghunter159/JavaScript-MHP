a4688bb4482b01a21107732d81ec216a33b03f07
mghunter159 <ya@mghunter.ru>
2023-04-16 22:44:09 +0500

    Urok14

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//localStorage.setItem('data', 5);
+/*//localStorage.setItem('data', 5);
 //console.log(localStorage.getItem('data'));
 
 let a = [1,2,3];
@@ -29,4 +29,54 @@ xhttp.open('GET', 'https://equeim.ru/', true)
 xhttp.send();
 function myfunc(data) {
     console.log(data);
-}
+}*/
+
+//Долгий
+/*fetch('http://127.0.0.1:5500/index.html')
+.then(data =>{
+    console.log(data)
+    data.text().then(data2 => {
+        console.log(data2)
+    })
+})*/
+
+//Средний
+/*fetch('http://127.0.0.1:5500/index.html')
+.then(data =>{
+    console.log(data)
+    return data.text()
+})
+.then(data =>{
+    console.log(data)
+})*/
+
+//Самый вариант
+/*fetch('http://127.0.0.1:5500/index.html')
+.then(data => data.text())
+.then(data => console.log(data))*/
+
+
+let a = new Promise((resolve, reject) => {
+    fetch('http://127.0.0.1:5500/index.html')
+    .then(data =>{
+        resolve(data.text())
+    })
+})
+
+let b = new Promise((resolve, reject) => {
+    fetch('http://127.0.0.1:5500/index.html')
+    .then(data =>{
+        resolve(data) 
+    })
+})
+
+/*a.then(data => {
+    console.log(data)
+})*/
+
+Promise.all([a,b]).then(data => {
+    console.log(data)
+})
+
+
+
